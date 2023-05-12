214255a9252116000a1ffd7f90b44cbb51ee80e3
Tedi-Septiawan <tediseptiawan32@gmail.com>
2023-05-12 22:29:23 +0700

    ok
    perubahan

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 //------------------------------::::::::::::::::::::------------------------------\\
-// Dibuat oleh di PT. Pacifica Raya Technology \\
+// Dibuat oleh FA Team klompok \\
 //------------------------------::::::::::::::::::::------------------------------\\
 
 // Arahkan user ketika pertama kali membuka applikasi ke halaman login
--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 //------------------------------::::::::::::::::::::------------------------------\\
-// Dibuat oleh FA Team di PT. Pacifica Raya Technology \\
+// Dibuat oleh FA Team klompok \\
 //------------------------------::::::::::::::::::::------------------------------\\
 ?>
 <!DOCTYPE html>
